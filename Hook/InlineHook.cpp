08328d338cc511e32a7bc62434b328287465ff5a
ainihw <2535836763@qq.com>
2021-10-26 16:15:45 +0800

    inline hook安全

--- Hook/InlineHook.cpp
+++ Hook/InlineHook.cpp
@@ -1,9 +1,11 @@
 //Inlinehook
-#include <Windows.h>
-DWORD JmpBackAddr;
+#include <Windows.h>	
+#include <TlHelp32.h>
+
+BYTE		bOldCode[0x5];	//Hook前的字节码
 int WINAPI My_MessageBox(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
 int	SetInlineHook(HMODULE hModule, PVOID FuncAddress, DWORD * pChangeAddress);
-int WINAPI Trampoline(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
+int DeleteHook(DWORD ChangeAddress);
 
 int main()
 {
@@ -19,7 +21,7 @@ int main()
 
 
 	//卸载InlineHook
-	//DeleteHook();
+	DeleteHook(ChangeAddress);
 	MessageBox(NULL, TEXT("InlineHook卸载成功"), NULL, MB_OK);
 
 	return 0;
@@ -29,51 +31,105 @@ int main()
 
 int	SetInlineHook(HMODULE hModule, PVOID FuncAddress, DWORD * pChangeAddress)
 {
-	HANDLE hProcess=GetCurrentProcess();			//获取当前进程句柄
-	int n = GetLastError();
-	*pChangeAddress = (DWORD)GetProcAddress(hModule,"MessageBoxA");					//安装Hook处的地址
-	JmpBackAddr = (*pChangeAddress) + 5;
-	BYTE WriteData[5];
+	//获取当前进程句柄
+	HANDLE hProcess=GetCurrentProcess();						
+	//安装Hook处的地址
+	*pChangeAddress = (DWORD)GetProcAddress(hModule,"MessageBoxA");	
+	
 
+	//读取HOOK前机器码
+	SIZE_T ByteNume = 0;
+	ReadProcessMemory(hProcess, LPVOID(*pChangeAddress), bOldCode, 0x5, &ByteNume);
+	if (!ByteNume)
+	{
+		return FALSE;
+	}
+
+	//向Hook位置写入jmp指令
+	BYTE		WriteData[5] = { 0 };		//需要写入的jmp指令
+	DWORD	dwOldProtect = 0;			//内存原来的属性
 	WriteData[0] = 0xE9;
-	((DWORD *)(WriteData + 1))[0] = (DWORD)FuncAddress - (*pChangeAddress) - 5;		//计算写入的jmp指令的偏移
+	((DWORD *)(WriteData + 1))[0] = (DWORD)FuncAddress - (*pChangeAddress) - 5;	
+	VirtualProtect(LPVOID(*pChangeAddress), 0x5, PAGE_EXECUTE_READWRITE, &dwOldProtect);
 
-	WriteProcessMemory(hProcess,LPVOID(*pChangeAddress), WriteData, 5, NULL);		//向Hook位置写入jmp指令
+	//HOOK操作的线程安全，暂停其他无关线程
+	HANDLE hThread = NULL;
+	THREADENTRY32 stThreadEntry32 = { 0 };
+	stThreadEntry32.dwSize = sizeof(THREADENTRY32);
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, GetCurrentProcessId());
+	Thread32First(hSnapshot, &stThreadEntry32);
+	do {
+		if (stThreadEntry32.th32ThreadID != GetCurrentThreadId())
+		{
+			hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, stThreadEntry32.th32ThreadID);
+			SuspendThread(hThread);
+			CloseHandle(hThread);
+		}
+	} while (TRUE == Thread32Next(hSnapshot, &stThreadEntry32));
+	CloseHandle(hSnapshot);
+	
+	WriteProcessMemory(hProcess,LPVOID(*pChangeAddress), WriteData, 5, NULL);		
+	
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, GetCurrentProcessId());
+	Thread32First(hSnapshot, &stThreadEntry32);
+	do {
+		if (stThreadEntry32.th32ThreadID != GetCurrentThreadId())
+		{
+			hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, stThreadEntry32.th32ThreadID);
+			ResumeThread(hThread);
+			CloseHandle(hThread);
+		}
+	} while (TRUE == Thread32Next(hSnapshot, &stThreadEntry32));
+	CloseHandle(hSnapshot);
+
+	VirtualProtect(LPVOID(*pChangeAddress), 0x5, dwOldProtect, &dwOldProtect);
+
+	return TRUE;
+}
+
+
+int DeleteHook(DWORD ChangeAddress)
+{
+
+	//获取当前进程句柄
+	HANDLE hProcess = GetCurrentProcess();
+	
+	//还原hook的指令
+	DWORD	dwOldProtect = 0;			//内存原来的属性
+	VirtualProtect(LPVOID(ChangeAddress), 0x5, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+	WriteProcessMemory(hProcess, LPVOID(ChangeAddress), bOldCode, 0x5, NULL);
+	VirtualProtect(LPVOID(ChangeAddress), 0x5, dwOldProtect, &dwOldProtect);
 
 	return 0;
 }
 
 
 
-
-
-
-
-
 //Detour函数
 int WINAPI My_MessageBox(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType)
 {
-
-	
-
-	int ret = Trampoline(hWnd, "InlineHook安装成功", lpCaption, uType);	//通过调用中间函数来达到直接调用原始MessageBox的目的
-	return ret;
-}
-
-
-//当调用原始MessageBox时直接调用此函数
-//此函数为裸函数（编译器不加优化）
-__declspec( naked )
-int WINAPI Trampoline(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType)
-{
+	//保存环境
 	_asm
 	{
-		//由于我们写入的Jmp指令破坏了原来的前3条指令,因此在这里执行原函数的前3条指令
-		mov edi,edi  //这一句其实可以不要
-		push ebp
-		mov ebp,esp
-		jmp JmpBackAddr //跳到Hook代码之后的地方，绕过自己安装的HOOK
-
+		pushad
+		pushfd
 	}
+
+	int		iRet = 0;
+	HMODULE	hModule = GetModuleHandle("USER32.dll");
+	DWORD	ChangeAddress = (DWORD)GetProcAddress(hModule, "MessageBoxA");
+
+	//先还原hook的函数，再调用
+	DeleteHook(ChangeAddress);
+	//恢复环境
+	_asm
+	{
+		popfd
+		popad
+	}
+	iRet = MessageBox(hWnd, "InlineHook安装成功", lpCaption, uType);		
+	SetInlineHook(hModule, My_MessageBox, &ChangeAddress);
+
+	return iRet;
 
 }
