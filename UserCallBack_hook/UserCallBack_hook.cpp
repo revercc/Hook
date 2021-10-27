a7f8c1907d374773335b1d7862c88e1cf2a51d71
revercc <2535836763@qq.com>
2021-10-27 14:56:14 +0800

    KernelCallbackTable  Hook

---
+++ UserCallBack_hook/UserCallBack_hook.cpp
@@ -0,0 +1,246 @@
+﻿// UserCallBack_hook.cpp : 定义应用程序的入口点。
+//KernelCallbackTable hook(hook USER_ClientLoadLibrary拦截全局消息钩子注入dll
+
+#include "framework.h"
+#include "UserCallBack_hook.h"
+
+#pragma pack(1)
+struct LOADHOOK
+{
+    BYTE    Reserved1[0x1c];
+    LPWSTR  lpDllName;
+    BYTE    Reserved2[0x4];
+};
+
+typedef  int(WINAPI *USER_ClientLoadlibrary)(LOADHOOK *);
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+DWORD dwOldFunc;                               //旧的函数的地址
+
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+int SetHook();
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_USERCALLBACKHOOK, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_USERCALLBACKHOOK));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_USERCALLBACKHOOK));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_USERCALLBACKHOOK);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+// 
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        //安装hook
+        SetHook();          
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+
+
+
+
+//Detour函数(User_ClientLoadlibrary)
+int WINAPI My_ClientLoadlibrary(LOADHOOK* lpHook)
+{
+    int iRet = 0;
+    USER_ClientLoadlibrary user_ClientLoadLibrary = (USER_ClientLoadlibrary)dwOldFunc;
+
+    //拦截指定DLL的加载
+    if (!wcscmp(lpHook->lpDllName, L"C:\\KEY.dll"))
+        lpHook->lpDllName = NULL;
+
+    //调用原函数
+    iRet = user_ClientLoadLibrary(lpHook);
+    
+    return iRet;
+}
+
+
+
+
+//设置UserCallback_hook
+//可以通过直接更改KernelCallBackTable中对应函数的地址，也可以直接通过inline hook对应函数
+int SetHook()
+{
+    DWORD dwOldProtect = 0;
+    DWORD dwKernelCallbackTable = 0;
+    
+    //获取KernelCallbackTable的值
+    _asm
+    {
+        mov eax,fs:[0x18]
+        mov eax,ds:[eax + 0x30]
+        mov eax,ds:[eax + 0x2c]
+        mov dwKernelCallbackTable,eax
+    }
+    
+    //保存旧的函数的地址
+    dwOldFunc = *(DWORD*)(dwKernelCallbackTable + 0x4b * 4);
+    //修改对应函数地址
+
+    VirtualProtect((LPVOID)(dwKernelCallbackTable + 0x4b *4), 0x4, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+    *(DWORD*)(dwKernelCallbackTable + 0x4b * 4) = (DWORD)My_ClientLoadlibrary;
+    VirtualProtect((LPVOID)(dwKernelCallbackTable + 0x4b * 4), 0x4, dwOldProtect, &dwOldProtect);
+
+    return TRUE;
+}
